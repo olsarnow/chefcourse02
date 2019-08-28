df03728e1e2c77a9ca55e2dc3d229d980039ed59
Olaf Sarnow <olsarnow@gmail.com>
2019-08-28 15:20:49 +0000

    chapter 1 completed

---
+++ setup.rb
@@ -0,0 +1,16 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+  content 'This server is the property of Olaf Sarnow'
+  action :create
+  owner 'root'
+  group 'root'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
